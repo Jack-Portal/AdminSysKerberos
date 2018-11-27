05e3f4f64d8939b4ba2af90d2847fb3dfa350f91
QuentainS <quentain.schietecatte@student.unamur.be>
2018-11-27 16:06:49 +0100

    First file

--- install.sh
+++ install.sh
@@ -1,10 +1,20 @@
 #!/bin/bash
 
 add-apt-repository universe
-apt-get update && apt-get upgrade
+apt-get update && apt-get -y upgrade
 apt install krb5-kdc krb5-admin-server
 
-krb5_newrealm
+echo "kerberos" | krb5_newrealm
+
 kadmin.local
-test
+# kerberos/admin
+# 2 * MDP
+# quit
+
+
+systemctl restart krb5-admin-server.service
+
+klist
+
+
 
