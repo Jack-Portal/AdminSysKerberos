b1d443246738321344cd2b53c1f56073044dacb8
QuentainS <quentain.schietecatte@student.unamur.be>
2018-11-28 01:17:06 +0100

    Finish the script

--- install.sh
+++ install.sh
@@ -6,8 +6,12 @@ apt install krb5-kdc krb5-admin-server -y
 
 krb5_newrealm
 
+echo " "
+echo " "
 echo "MEMO : addprinc kerberos/admin"
 echo "Once the password confirmed, enter \"quit\""
+echo " "
+echo " "
 
 kadmin.local
 
