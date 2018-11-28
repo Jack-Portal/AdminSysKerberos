0c3f9807dcd71d1bd9e04b088dd0239bb50418cf
QuentainS <quentain.schietecatte@student.unamur.be>
2018-11-28 14:54:37 +0100

    Add comments / finish scripts

--- client.sh
+++ client.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
 
+# Temporarily add the universe repository 
 add-apt-repository universe
 
+# Install the packages
 apt-get update -y && apt-get upgrade -y
-apt-get install krb5-user libpam-krb5 libpam-ccreds auth-client-config -y
+apt-get install krb5-user libpam-krb5 libpam-ccreds -y
 
+# Configure the client
 sudo dpkg-reconfigure krb5-config
 
+# Configure the libpam-krb5 module to ask ticket on a session opening
+auth-client-config -a -p kerberos_example
 
+# Disable the universe repository
+add-apt-repository --remove universe
 
 echo " "
 echo "The installation is finished !"
--- install.sh
+++ install.sh
@@ -1,22 +1,30 @@
 #!/bin/bash
 
+# Temporarily add the universe repository
 add-apt-repository universe
 
+# Install the packages
 apt-get update -y && apt-get upgrade -y
 apt install krb5-kdc krb5-admin-server -y
 
+# Create a new domain (realm)
 krb5_newrealm
 
 echo " "
 echo " "
-echo "MEMO : addprinc kerberos/admin"
+echo "MEMO : addprinc <user>/admin"
 echo "Once the password confirmed, enter \"quit\""
 echo " "
 echo " "
 
+# Summon the kadmin shell in order to set up the admin user
 kadmin.local
 
+# Restart the kerberos service
 systemctl restart krb5-admin-server.service
+
+# Disable the universe repository
+add-apt-repository --remove universe
 
 echo " "
 echo "The installation is finished !"
